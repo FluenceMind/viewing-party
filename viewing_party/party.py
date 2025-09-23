f252a31534aba5574f4986fa518cf6973e554c76
Makiko <trimakichan@gmail.com>
2025-09-23 14:10:45 -0700

    complete wave 2 and passed all the tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -58,6 +58,47 @@ def watch_movie(user_data, title):
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
 
+def get_watched_avg_rating(user_data):
+    """
+    Average rating of movies in user_data["watched"].
+    If the list is empty, return 0.0.
+    """
+    total_ratings = 0.0
+
+    watched = user_data.get("watched", [])
+    if not watched:
+        return total_ratings
+    
+    for movie in watched:
+        rating = movie["rating"]
+        total_ratings += rating
+    
+    ave_rating = total_ratings / len(watched)
+
+    return ave_rating
+
+
+def get_most_watched_genre(user_data):
+    """
+    Find the most common genre in watched movies.
+    If watched is empty, return None.
+    """
+
+    if not user_data["watched"]:
+        return None
+    
+    genre_count = {} 
+    max_count = -1
+    most_watched_genre = None
+
+    for movie in user_data["watched"]:
+        genre = movie["genre"]
+        genre_count[genre] = genre_count.get(genre, 0) + 1
+        if genre_count[genre] > max_count:
+            max_count = genre_count[genre]
+            most_watched_genre = genre
+
+    return most_watched_genre
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
